bcf7c70212d2dca534c31f1c7a3e6ce807923640
SakiRinn <sakirinn@qq.com>
2023-06-10 19:40:14 +0800

    delete redundant comment.

--- src/socket.c
+++ src/socket.c
@@ -48,10 +48,6 @@ ssize_t udp_receive(int sock, struct sockaddr_in *client_addr, char *buffer) {
     return receive_len;
 }
 
-/**
-* @brief Create a TCP socket.
-* @return socket on success - 1 on
-*/
 int tcp_socket() {
     int sock = socket(PF_INET, SOCK_STREAM, 0);
     // Creates a new TCP socket.
@@ -62,11 +58,6 @@ int tcp_socket() {
     return sock;
 }
 
-/**
-* @brief Connects a socket to a destination
-* @param sock the socket to connect to
-* @param dest_addr the address to connect
-*/
 void tcp_connect(int sock, struct sockaddr_in *dest_addr) {
     // Connect to the TCP server.
     if (connect(sock, (struct sockaddr *)dest_addr,
@@ -76,10 +67,6 @@ void tcp_connect(int sock, struct sockaddr_in *dest_addr) {
     }
 }
 
-/**
-* @brief Set listen mode on socket
-* @param sock socket to set listen
-*/
 void tcp_listen(int sock) {
     // listen for TCP connections on the socket
     if (listen(sock, LISTEN_BACKLOG) == -1) {
@@ -88,12 +75,6 @@ void tcp_listen(int sock) {
     }
 }
 
-/**
-* @brief Accepts a connection on the socket.
-* @param sock The socket to accept on
-* @param client_addr * A pointer to an address to store the address of the client
-* @return Returns the socket on success - 1 on
-*/
 int tcp_accept(int sock, struct sockaddr_in *client_addr) {
     socklen_t client_addr_len = sizeof(struct sockaddr_in);
     int client_sock =
@@ -106,12 +87,6 @@ int tcp_accept(int sock, struct sockaddr_in *client_addr) {
     return client_sock;
 }
 
-/**
-* @brief Send data over a TCP socket
-* @param client_sock Socket to send data over
-* @param buffer Buffer containing data to send
-* @param buffer_len Length of the data
-*/
 void tcp_send(int client_sock, char *buffer, size_t buffer_len) {
     // Send a packet to the client.
     if (send(client_sock, buffer, buffer_len, 0) != buffer_len) {
@@ -119,12 +94,6 @@ void tcp_send(int client_sock, char *buffer, size_t buffer_len) {
     }
 }
 
-/**
-* @brief Receive data from client.
-* @param client_sock socket to receive data from
-* @param buffer * buffer to store received data
-* @return number of bytes received or - 1 on
-*/
 ssize_t tcp_receive(int client_sock, char *buffer) {
     ssize_t receive_len = recv(client_sock, buffer, BUFSIZE, 0);
     // receive_len 0 if receive_len 0 exit with exit code EXIT_FAILURE
